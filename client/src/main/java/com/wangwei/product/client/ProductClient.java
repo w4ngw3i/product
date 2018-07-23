848683d0c190d705a3a90611a3c95da7e0a08e8c
wangwei <china.wangwei@icloud.com>
2018-07-23 11:45:07 +0800

    add new file

---
+++ client/src/main/java/com/wangwei/product/client/ProductClient.java
@@ -0,0 +1,25 @@
+package com.wangwei.product.client;
+
+import com.wangwei.product.common.DecreaseStockInput;
+import com.wangwei.product.common.ProductInfoOutput;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+/**
+ * @Auther wangwei
+ * @Date 2018/4/10 下午1:52
+ */
+@FeignClient(name = "product")
+public interface ProductClient {
+
+    @PostMapping("/product/listForOrder")
+    List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList);
+
+    @PostMapping("/product/decreaseStock")
+    void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+}
